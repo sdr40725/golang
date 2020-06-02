672ce74c0393238eda1134e6627bbf7fa7cb37a6
howard <sdr40725@gmail.com>
2020-06-02 09:39:57 +0800

    add file

--- main.go
+++ main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"fmt"
+	"github.com/sdr40725/golang/helloworld"
 )
 
 func main() {
 	a := 1
-	fmt.Printf(HelloWorld())
+	fmt.Printf(helloworld.HelloWorld())
 	if a > 1 {
 		fmt.Println("a > 1")
 	}
